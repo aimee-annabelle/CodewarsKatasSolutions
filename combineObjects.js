66d2aab417bdade3cac81a59925d9eb40b6d84f0
aimee-annabelle <inezannabelle@gmail.com>
2024-05-24 11:10:09 +0200

    done: combine objects

--- combineObjects.js
+++ combineObjects.js
@@ -1,11 +1,18 @@
 function combine() {
   let newObj = {};
-  let objArr = [];
+
   for (let argument of arguments) {
-    objArr.push({ ...argument });
+    for (let key in argument) {
+      if (argument.hasOwnProperty(key)) {
+        if (newObj.hasOwnProperty(key)) {
+          newObj[key] += argument[key];
+        } else {
+          newObj[key] = argument[key];
+        }
+      }
+    }
   }
-  objArr.reduce((curr, acc) => {});
-  console.log(objArr);
+
   return newObj;
 }
 const objA = { a: 10, b: 20, c: 30 };
@@ -13,4 +20,4 @@ const objB = { a: 3, c: 6, d: 3 };
 const objC = { a: 5, d: 11, e: 8 };
 const objD = { c: 3 };
 console.log(combine(objA, objB));
-// console.log(combine(objA, objC))
+console.log(combine(objA, objC));
