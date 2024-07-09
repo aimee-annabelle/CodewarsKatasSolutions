48f974b06cd41ffc8ab4abcdf0266f653f520c6e
aimee-annabelle <inezannabelle@gmail.com>
2024-07-09 23:27:25 +0200

    grouping files

---
+++ js/convert-temp.js
@@ -0,0 +1,254 @@
+/*
+    "C"  
+    "F"  
+    "K"  
+    "R"  
+    "De" 
+    "N"  
+    "Re" 
+    "Ro" 
+*/
+function convertTemp(temp, from_scale, to_scale){
+  switch (from_scale) {
+    case "C":
+        switch (to_scale) {
+            case "F": 
+                
+                break;
+            case "K": 
+
+                break;
+            case "R": 
+            
+                break;
+            case "De":
+                
+                break;
+            case "N": 
+                
+                break;
+            case "Re":
+                
+                break;
+            case "Ro":
+
+                break;
+            default:
+                break;
+        }
+        break; 
+    case "F":
+        switch (to_scale) {
+            case "C":
+                
+                break;
+            case "F": 
+                
+                break;
+            case "K": 
+
+                break;
+            case "R": 
+            
+                break;
+            case "De":
+                
+                break;
+            case "N": 
+                
+                break;
+            case "Re":
+                
+                break;
+            case "Ro":
+
+                break;
+            default:
+                break;
+        }
+        break;
+    case "K":
+        switch (to_scale) {
+            case "C":
+                
+                break;
+            case "F": 
+                
+                break;
+            case "K": 
+
+                break;
+            case "R": 
+            
+                break;
+            case "De":
+                
+                break;
+            case "N": 
+                
+                break;
+            case "Re":
+                
+                break;
+            case "Ro":
+
+                break;
+            default:
+                break;
+        }
+        break;
+    case "R": 
+    switch (to_scale) {
+        case "C":
+            
+            break;
+        case "F": 
+            
+            break;
+        case "K": 
+
+            break;
+        case "R": 
+        
+            break;
+        case "De":
+            
+            break;
+        case "N": 
+            
+            break;
+        case "Re":
+            
+            break;
+        case "Ro":
+
+            break;
+        default:
+            break;
+    }
+        break;
+    case "De": 
+    switch (to_scale) {
+        case "C":
+            
+            break;
+        case "F": 
+            
+            break;
+        case "K": 
+
+            break;
+        case "R": 
+        
+            break;
+        case "De":
+            
+            break;
+        case "N": 
+            
+            break;
+        case "Re":
+            
+            break;
+        case "Ro":
+
+            break;
+        default:
+            break;
+    }
+        break;
+    case "N":
+        switch (to_scale) {
+            case "C":
+                
+                break;
+            case "F": 
+                
+                break;
+            case "K": 
+
+                break;
+            case "R": 
+            
+                break;
+            case "De":
+                
+                break;
+            case "N": 
+                
+                break;
+            case "Re":
+                
+                break;
+            case "Ro":
+
+                break;
+            default:
+                break;
+        }
+        break;  
+    case "Re":
+        switch (to_scale) {
+            case "C":
+                
+                break;
+            case "F": 
+                
+                break;
+            case "K": 
+
+                break;
+            case "R": 
+            
+                break;
+            case "De":
+                
+                break;
+            case "N": 
+                
+                break;
+            case "Re":
+                
+                break;
+            case "Ro":
+
+                break;
+            default:
+                break;
+        }
+        break; 
+    case "Ro":
+        switch (to_scale) {
+            case "C":
+                
+                break;
+            case "F": 
+                
+                break;
+            case "K": 
+
+                break;
+            case "R": 
+            
+                break;
+            case "De":
+                
+                break;
+            case "N": 
+                
+                break;
+            case "Re":
+                
+                break;
+            case "Ro":
+
+                break;
+            default:
+                break;
+        }
+        break;
+        
+    default:
+        break;
+  }
+}
