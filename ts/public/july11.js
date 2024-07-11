fa3504d624c5d6f8f4a7b58d3287063888a1b4e2
aimee-annabelle <inezannabelle@gmail.com>
2024-07-11 20:52:33 +0200

    codewars

---
+++ ts/public/july11.js
@@ -0,0 +1,27 @@
+"use strict";
+// Credit Card Mask
+function maskify(cc) {
+    if (cc.length < 4) {
+        return cc;
+    }
+    else {
+        let replaced = cc
+            .split("")
+            .slice(0, cc.length - 4)
+            .map((el) => (el = "#"))
+            .join("");
+        return (replaced +
+            cc
+                .split("")
+                .slice(cc.length - 4)
+                .join(""));
+    }
+}
+// 3 powers of 2
+/** Write a function three_powers() to accept a number,
+ * to check can it represent as sum of 3 powers of 2.
+ * (n == 2**i + 2**j + 2**k, i, j, k >= 0)
+ * */
+function threePowers(n) {
+    return false;
+}
