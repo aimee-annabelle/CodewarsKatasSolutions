bc0365c8f29c4bbf98c9dcbec40fc0db3c760b47
aimee-annabelle <inezannabelle@gmail.com>
2024-07-17 15:24:24 +0200

    online cartesian points

--- ts/public/july17.js
+++ ts/public/july17.js
@@ -3,7 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.doubleChar = doubleChar;
 exports.updateLight = updateLight;
 function doubleChar(str) {
-    const double = str.split('').map(el => el.repeat(2)).join('');
+    const double = str
+        .split("")
+        .map((el) => el.repeat(2))
+        .join("");
     return double;
 }
 doubleChar("abcd");
@@ -26,3 +29,38 @@ function updateLight(current) {
             return _exhaustiveCheck;
     }
 }
+function oneLine(points) {
+    const uniquePoints = points.filter((point, index, self) => index === self.findIndex((p) => p[0] === point[0] && p[1] === point[1]));
+    if (uniquePoints.length < 3) {
+        return true;
+    }
+    const x = uniquePoints[0];
+    const y = uniquePoints[1];
+    for (let i = 2; i < uniquePoints.length; i++) {
+        const z = uniquePoints[i];
+        if ((y[0] - x[0]) * (z[1] - x[1]) !== (y[1] - x[1]) * (z[0] - x[0])) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(oneLine([
+    [1, 2],
+    [7, 4],
+    [22, 9],
+]));
+console.log(oneLine([
+    [74, 6],
+    [4, -1],
+    [-96, -11],
+    [14, 0],
+    [-76, -9],
+    [-106, -12],
+    [44, 3],
+]));
+console.log(oneLine([
+    [1, 1],
+    [1, 1],
+    [0, 0],
+    [1, -1],
+]));
--- ts/src/july17.ts
+++ ts/src/july17.ts
@@ -1,24 +1,75 @@
-export function doubleChar(str: string): string{
-    const double = str.split('').map(el => el.repeat(2)).join('') 
-    return double
-  }
+export function doubleChar(str: string): string {
+  const double = str
+    .split("")
+    .map((el) => el.repeat(2))
+    .join("");
+  return double;
+}
 doubleChar("abcd");
 
 enum lights {
-    green = 'green',
-    yellow = 'yellow',
-    red = 'red'
- }
-export function updateLight(current: lights): string {
- switch (current) {
-    case lights.green:
-        return lights.yellow      
-    case lights.yellow:
-        return lights.red        
-    case lights.red:
-        return lights.green
-    default:
-        const _exhaustiveCheck: never = current;
-        return _exhaustiveCheck;
- }
+  green = "green",
+  yellow = "yellow",
+  red = "red",
 }
+export function updateLight(current: lights): string {
+  switch (current) {
+    case lights.green:
+      return lights.yellow;
+    case lights.yellow:
+      return lights.red;
+    case lights.red:
+      return lights.green;
+    default:
+      const _exhaustiveCheck: never = current;
+      return _exhaustiveCheck;
+  }
+}
+
+function oneLine(points: [number, number][]) {
+  const uniquePoints = points.filter(
+    (point, index, self) =>
+      index === self.findIndex((p) => p[0] === point[0] && p[1] === point[1])
+  );
+
+  if (uniquePoints.length < 3) {
+    return true;
+  }
+
+  const x = uniquePoints[0];
+  const y = uniquePoints[1];
+
+  for (let i = 2; i < uniquePoints.length; i++) {
+    const z = uniquePoints[i];
+    if ((y[0] - x[0]) * (z[1] - x[1]) !== (y[1] - x[1]) * (z[0] - x[0])) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(
+  oneLine([
+    [1, 2],
+    [7, 4],
+    [22, 9],
+  ])
+);
+console.log(
+  oneLine([
+    [74, 6],
+    [4, -1],
+    [-96, -11],
+    [14, 0],
+    [-76, -9],
+    [-106, -12],
+    [44, 3],
+  ])
+);
+console.log(
+  oneLine([
+    [1, 1],
+    [1, 1],
+    [0, 0],
+    [1, -1],
+  ])
+);
